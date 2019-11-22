6cc0a7ebe2ad36b9cc57f10c9359d0d0c1167cef
unknown <liangfan2@iflytek.com>
2019-11-22 11:45:44 +0800

    create project

---
+++ personalCenter/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=10)
+    birthday = models.DateField()
+    address = models.CharField(max_length=30)
+
+class Follow(models.Model):
+    # 两个相同的外键，通过related_name区分,Follow.idol.all()和Follow.fans.all()获取
+    idol = models.ForeignKey(User, on_delete=models.CASCADE, related_name='idol')
+    fans = models.ForeignKey(User, on_delete=models.CASCADE, related_name='fans')
+    followTime = models.DateTimeField(auto_now_add=True) #自动设置添加时间
+
+
