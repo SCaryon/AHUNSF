31e4f2c88b36bc314fc011e3baf0e89f42fcb67e
unknown <liangfan2@iflytek.com>
2019-11-22 17:05:53 +0800

    finish register and login

--- personalCenter/urls.py
+++ personalCenter/urls.py
@@ -6,6 +6,7 @@ app_name = 'personalCenter'
 urlpatterns = [
     path('<int:uid>', views.center, name='center'),
     path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
     path('follow/<int:uid>', views.follow, name='follow'),
     path('unfollow/<int:uid>', views.unfollow, name='unfollow'),
 ]
--- personalCenter/views.py
+++ personalCenter/views.py
@@ -1,6 +1,7 @@
 import datetime
+import base64
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .models import User
 
 
@@ -9,14 +10,10 @@ from .models import User
 # /user/register
 def register(request):
     '''
-    request 使用POST传入字段
-       email
-       name
-       password
-       year,month,day  (允许为空）
-       address (允许为空）
+    :param request: email name password year,month,day  (允许为空） address (允许为空）
+    :return:
     '''
-    content = {}
+    context = {}
     email = request.POST.get('email')
     name = request.POST.get('name')
     password = request.POST.get('password')
@@ -26,31 +23,59 @@ def register(request):
     address = request.POST.get('address')
     birthday = None
     user_obj = User.objects.filter(email=email).first()
-    if(user_obj!=None):
+    print(user_obj)
+    if (user_obj == None):
         if (len(year) != 0 & len(month) != 0 & len(day) != 0):
             birthday = datetime.date(year, month, day)
+        # 对密码进行加密
+        password = base64.b64encode(bytes(password, 'ascii'))
         user = User(email=email, name=name, password=password, birthday=birthday, address=address)
         user.save()
         content['status'] = 1
-        return HttpResponse(content)
+        content['message'] = "添加用户成功"
+        return JsonResponse(context, json_dumps_params={'ensure_ascii': False})
     else:
-        return HttpResponse("该邮箱已注册")
+        content['status'] = 0
+        content['message'] = "添加用户失败"
+        return JsonResponse(context, json_dumps_params={'ensure_ascii': False})
 
+
+# /user/register
+def login(request):
+    '''
+
+    :param request: email password
+    :return:
+    '''
+    context = {}
+    email = request.POST.get('email')
+    password = base64.b64encode(bytes(request.POST.get('password'), 'ascii'))
+    user_obj = User.objects.filter(email=email).first()
+    if (user_obj == None):
+        context['status'] = 0
+        context['message'] = "该用户未注册"
+    elif (user_obj.password != password):
+        context['status'] = 0
+        context['message'] = "密码错误"
+    else:
+        context['status'] = 1
+        context['message'] = "登录成功"
+    return JsonResponse(context, json_dumps_params={'ensure_ascii': False})
 
 
 # /user/uid
 def center(request, uid):
-    content = '个人中心'
-    return HttpResponse(content + str(uid))
+    context = {'message': '个人中心'}
+    return JsonResponse(context, json_dumps_params={'ensure_ascii': False})
 
 
 # /user/unfollow/uid  uid被删除的idol
 def unfollow(request, uid):
-    content = '取消关注'
-    return HttpResponse(content + str(uid))
+    context = {'message': '取消关注'}
+    return JsonResponse(context, json_dumps_params={'ensure_ascii': False})
 
 
 # /user/follow/uid uid添加的idol
 def follow(request, uid):
-    content = '添加关注'
-    return HttpResponse(content + str(uid))
+    context = {'message': '添加关注'}
+    return JsonResponse(context, json_dumps_params={'ensure_ascii': False})
