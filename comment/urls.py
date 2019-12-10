36f168ce5384e7705c4d848aa64ef191b9a1040e
feynman1999 <chenyx.cs@gmail.com>
2019-12-10 13:56:38 +0800

    完成了评论的删除

--- comment/urls.py
+++ comment/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 # start with /comment/
 urlpatterns = [
-    path('update_comment', update_comment , name='update_comment'),
+    path('update_comment', update_comment, name='update_comment'),
+    path('delete_comment', delete_comment, name='delete_comment'),
 ]
